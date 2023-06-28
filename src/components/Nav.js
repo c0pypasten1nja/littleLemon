5731a298b6b438ce48937dca8d0a5769935567fe
c0pypasten1nja <yeowian@gmail.com>
2023-06-28 23:56:23 +0800

    Break sections down into React components

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+function Nav() {
+    return (
+      <nav>
+        <a>Home</a>
+        <a>About</a>
+        <a>Menu</a>
+        <a>Reservations</a>
+        <a>Order Online</a>
+        <a>Login</a>
+      </nav>
+    );
+  }
+
+  export default Nav;
+  
